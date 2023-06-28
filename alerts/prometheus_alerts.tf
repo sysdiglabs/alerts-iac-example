84c994fffd1b8ca536792ea863e355600b0bc86d
Patrick Easters <patrick.easters@sysdig.com>
2023-06-28 00:35:37 -0400

    Add missing group alert

--- alerts/prometheus_alerts.tf
+++ alerts/prometheus_alerts.tf
@@ -6,3 +6,11 @@ resource "sysdig_monitor_alert_v2_prometheus" "test_alert" {
     query = "rate(kube_pod_container_status_restarts_total{kube_cluster_name=\"prod\"}[5m])>0"
     trigger_after_minutes = 10
 }
+
+resource "sysdig_monitor_alert_v2_prometheus" "missing_group" {
+    name = "probe up"
+    description = "scrape job uptime"
+    severity = "medium"
+    query = "min(up) < 1"
+    trigger_after_minutes = 10
+}

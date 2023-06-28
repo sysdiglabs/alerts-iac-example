6fe713e205b6468df3b274f5a440194dba58c4ba
Patrick Easters <patrick.easters@sysdig.com>
2023-06-27 23:24:41 -0400

    Add failing alert

--- alerts/prometheus_alerts.tf
+++ alerts/prometheus_alerts.tf
@@ -6,3 +6,11 @@ resource "sysdig_monitor_alert_v2_prometheus" "test_alert" {
     query = "rate(kube_pod_container_status_restarts_total{kube_cluster_name=\"prod\"}[5m])>0"
     trigger_after_minutes = 10
 }
+
+resource "sysdig_monitor_alert_v2_prometheus" "bad_alert" {
+    name = "container restarts"
+    description = "scrape job uptime"
+    severity = "high"
+    query = "rate(kube_pod_container_status_restarts_total{kube_cluster_name=\"prod\"}[5m])>0"
+    trigger_after_minutes = 10
+}
